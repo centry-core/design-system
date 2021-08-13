0329a95b177bb170dcbc8ed9234000bc5ec3d082
ad13box@gmail.com <ad13box@gmail.com>
2021-08-13 14:29:23 +0300

    Fixed navigation Added css for colored-select Changed select js

--- assets/js/select.js
+++ assets/js/select.js
@@ -1,23 +1,18 @@
-function paintSelect(el) {
-    var className = $(el.children[el.selectedIndex]).attr('class')
-    var buttonStyle = el.nextSibling;
-    switch (className) {
-        case 'critical':
-            $(buttonStyle).css({ "border-color": "#FFF0F0", "color": "#F32626", "background-color": "#FFF0F0" });
-            break;
-        case 'high':
-            $(buttonStyle).css({ "border-color": "#FFEDDD", "color": "#E97912", "background-color": "#FFEDDD" });
-            break;
-        case 'medium':
-            $(buttonStyle).css({ "border-color": "#FFFBE7", "color": "#DBC714", "background-color": "#FFFBE7" });
-            break;
-        case 'low':
-            $(buttonStyle).css({ "border-color": "#E7FFE7", "color": "#18B64D", "background-color": "#E7FFE7" });
-            break;
-        case 'info':
-            $(buttonStyle).css({ "border-color": "#E0F2FE", "color": "#2F7DF1", "background-color": "#E0F2FE" });
-            break;
-    }
+const coloredClassNameRegexp = new RegExp(/colored-select-(\w+)/, 'i');
+const palette = {
+    red: {"border-color": "#FFF0F0", "color": "#F32626", "background-color": "#FFF0F0"},
+    orange: {"border-color": "#FFEDDD", "color": "#E97912", "background-color": "#FFEDDD"},
+    yellow: {"border-color": "#FFFBE7", "color": "#DBC714", "background-color": "#FFFBE7"},
+    green: {"border-color": "#E7FFE7", "color": "#18B64D", "background-color": "#E7FFE7"},
+    blue: {"border-color": "#E0F2FE", "color": "#2F7DF1", "background-color": "#E0F2FE"},
+    default: {"border-color": "inherit", "color": "inherit", "background-color": "inherit"},
+}
+
+const paintSelect = el => {
+    const className = $(el.children[el.selectedIndex]).attr('class');
+    const color = className ? className.match(coloredClassNameRegexp)[1] : 'default';
+    const style = palette[color] || palette['default'];
+    $(el.nextSibling).css(style);
 }
 
 $(".btn-colored-select").on("changed.bs.select", function (e, clickedIndex, newValue, oldValue) {
