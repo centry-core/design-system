7fb79d73c6b36151692b7dd63072a9832c96f31d
Artem Rozumenko <artyom.rozumenko@gmail.com>
2021-09-13 12:47:58 +0300

    added params control

--- assets/js/table.js
+++ assets/js/table.js
@@ -8,6 +8,41 @@ function lgFormatter(value, row, index) {
     }
 }
 
+function inputFormatter(value, row, index, field) {
+    return `<input type="text" class="form-control form-control-alternative" onchange="updateCell(this, ${index}, '${field}')" value="${value}">`
+}
+
+function updateCell(el, row, field) {
+    $(el.closest('table')).bootstrapTable('updateCell', {index: row, field: field, value: el.value})
+}
+
+function dataTypeFormatter(value, row, index, field) {
+    var content = `<select class="selectpicker mr-2 datatype-selectpicker" data-style="btn-gray">`;
+    var types = ['String', 'Number', 'List'];
+    types.forEach(function(item){
+        if (value == item) {
+            content += `<option selected>${item}</option>`
+        } else {
+            content += `<option>${item}</option>`
+        }
+    })
+    content += "</select>"
+    return content
+}
+
+function addEmptyParamsRow(id) {
+    $(`#${id}`).bootstrapTable('append', {"name": "", "default": "", "type": "", "description": ""})
+}
+
+
+function parametersDeleteFormatter(value, row, index) {
+    return `
+    <div class="d-flex justify-content-end">
+        <button type="button" class="btn btn-16 btn-action" onclick="deleteParams(${index})"><i class="fas fa-trash-alt"></i></button>
+    </div>
+    `
+}
+
 function actionFormatter(value, row, index) {
     return `
     <div class="d-flex justify-content-end">
@@ -18,6 +53,20 @@ function actionFormatter(value, row, index) {
     </div>
     `
 }
+
+$('.params-table').on('all.bs.table', function (ev) {
+    $(ev.target).find('.selectpicker').each(function(index) {
+        $(this).selectpicker('render')
+        if ($('.params-table').bootstrapTable('getData')[parseInt($(this).closest('tr').attr('data-index'))].type != ""){
+            $(this).selectpicker('val', $('.params-table').bootstrapTable('getData')[parseInt($(this).closest('tr').attr('data-index'))].type)
+        }
+    })
+    $(".datatype-selectpicker").on('changed.bs.select', function (e, clickedIndex, isSelected, previousValue) {
+        if ($('.params-table').bootstrapTable('getData')[parseInt($(this).closest('tr').attr('data-index'))].type != this.value) {
+            updateCell(this, parseInt($(this).closest('tr').attr('data-index')), 'type')
+        }
+    })
+})
 
 function nameStyle(value, row, index) {
     return {css: {"max-width": "100px", "overflow": "hidden", "text-overflow": "ellipsis", "white-space": "nowrap"}}
