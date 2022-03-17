95307b908b2561fa687cbc536e0d0dd429c97f93
Vitalii_Kondratskyi <vitalii_kondratskyi@epam.com>
2022-03-17 11:06:09 +0200

    fix(sclor)

--- static/assets/js/wizard.js
+++ static/assets/js/wizard.js
@@ -45,5 +45,28 @@ $(document).ready(function() {
         $("#vuh").text(vuh);
         $("#cost").text(cost);
     })
+
+    noUiSlider.create($("#vuh-slider1")[0], {
+        start: 40000,
+        range: {
+            'min': 500,
+            'max': 60000
+        },
+        step: l500,
+        format: wNumb({
+            decimals: 0
+        }),
+        pips: {
+            mode: 'values',
+            values: [500, 5000, 20000, 40000, 60000],
+            density: 3
+        }
+    })
+    $("#vuh-slider1")[0].noUiSlider.on('update', function (values, handle, unencoded, isTap, positions) {
+        var vuh = parseInt(values[handle])
+        var cost = vuh === 500 ? "FREE" : `${vuh * 0.1} $`
+        $("#vuh").text(vuh);
+        $("#cost").text(cost);
+    })
     
 })
