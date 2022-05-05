6c88c17d9234394a027a657c4804a98cb09373f3
Vitalii_Kondratskyi <vitalii_kondratskyi@epam.com>
2022-05-05 16:47:27 +0300

    fix(REPORT_FILTER): remove row after changing status if active tab of filter

--- static/assets/js/report-filter.js
+++ static/assets/js/report-filter.js
@@ -144,8 +144,6 @@ const ReportFilter = {
     mounted() {
         this.editableFilter = deepClone(this.selectedFilter);
         this.renderSelect();
-        const event = new Event('vue_init')
-        document.dispatchEvent(event);
     },
     watch: {
         editableFilter: {
@@ -339,7 +337,12 @@ const vueApp = Vue.createApp({
                 data: this.tableData,
                 theadClasses: 'thead-light'
             }
-            $('#table').bootstrapTable(tableOptions)
+            $('#table').bootstrapTable(tableOptions);
+            document.addEventListener('select-event', (e) => {
+                if (this.activeTab !== 'all' && e.detail.field === 'status') {
+                    $('#table').bootstrapTable('hideRow', { index: e.detail.row })
+                }
+            }, false);
         },
         fetchTableData() {
             this.loadingTable = true;
