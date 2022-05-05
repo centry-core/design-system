314898c34dd8e4919c41cb1044aba7b31ac4b2f0
aspect13 <ad13box@gmail.com>
2022-05-05 15:10:00 +0200

    fixing security and some static addings

--- static/assets/js/table.js
+++ static/assets/js/table.js
@@ -8,13 +8,6 @@
 //     }
 // }
 
-function inputFormatter(value, row, index, field) {
-    return `
-        <input type="text" class="form-control form-control-alternative" onchange="updateCell(this, ${index}, '${field}')" value="${value}">
-        <div class="invalid-tooltip invalid-tooltip-custom"></div>
-    `
-}
-
 function updateCell(el, row, field) {
     $(el.closest('table')).bootstrapTable('updateCell', {index: row, field: field, value: el.value})
 }
@@ -67,60 +60,15 @@ const tableColoredSelectFormatter = (value, row, index, optionsList, fieldName) 
     `
 }
 
-function dataTypeFormatter(value, row, index, field) {
-    const options = ['String', 'Number', 'List'].map(item =>
-        `<option 
-            value=${item} 
-            ${item.toLowerCase() === value.toLowerCase() ? 'selected' : ''}
-        >
-            ${item}
-        </option>
-        `
-    )
-    return `
-        <select class="selectpicker mr-2" data-style="btn-gray" onchange="updateCell(this, '${index}', '${field}')">
-            ${options.join('')}
-        </select>
-    `
-}
-
-const addEmptyParamsRow = source => {
-    $(source).closest('.section').find('.params-table').bootstrapTable(
-        'append',
-        {"name": "", "default": "", "type": "string", "description": "", "action": ""}
-    )
-}
 
 
-function parametersDeleteFormatter(value, row, index) {
-    return `
-    <div class="d-flex justify-content-end">
-        <button type="button" class="btn btn-16 btn-action" onclick="deleteParams(${index}, this)"><i class="fas fa-trash-alt"></i></button>
-    </div>
-    `
-}
 
-function actionFormatter(value, row, index) {
-    return `
-    <div class="d-flex justify-content-end table-action">
-        <button type="button" class="btn btn-24 btn-action"><i class="fas fa-play"></i></button>
-        <button type="button" class="btn btn-24 btn-action"><i class="fas fa-cog"></i></button>
-        <button type="button" class="btn btn-24 btn-action"><i class="fas fa-share-alt"></i></button>
-        <button type="button" class="btn btn-24 btn-action"><i class="fas fa-trash-alt"></i></button>
-    </div>
-    `
-}
 
 function nameStyle(value, row, index) {
     return {css: {"max-width": "100px", "overflow": "hidden", "text-overflow": "ellipsis", "white-space": "nowrap"}}
 }
 
-const deleteParams = (index, source) => {
-    $(source).closest('.params-table').bootstrapTable('remove', {
-        field: '$index',
-        values: [index]
-    })
-}
+
 
 
 wait_for('bootstrapTable', jQuery.fn).then(v => (
