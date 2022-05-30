a79460ff46be95ab94ce2b4a9c40d8e97b10adb0
Vitalii_Kondratskyi <vitalii_kondratskyi@epam.com>
2022-05-30 21:09:05 +0300

    feat(CHART):added cards and tables

--- static/assets/js/dropdowns.js
+++ static/assets/js/dropdowns.js
@@ -361,6 +361,101 @@ const ComplexListFilter = {
         </div>`
 };
 
+const MultiselectFilter = {
+    props: ['title'],
+    data() {
+        return {
+            inputSearch: '',
+            itemsList: [...Array(5).keys()].map((item, index) => (
+                { id: Math.round(Math.random() * 1000), title: `Step ${index + 1}`}
+            )),
+            refSearchId: 'refSearchCbx'+Math.round(Math.random() * 1000),
+            selectedItems: [],
+            closeOnItem: true,
+        }
+    },
+    computed: {
+        foundItems() {
+            return this.inputSearch ?
+                this.itemsList.filter(item => item.title.toUpperCase().includes(this.inputSearch.toUpperCase())) :
+                this.itemsList
+        },
+        isAllSelected() {
+            return (this.selectedItems.length < this.itemsList.length) && this.selectedItems.length > 0
+        }
+    },
+    watch: {
+        selectedItems: function (val) {
+            this.$refs[this.refSearchId].checked = this.selectedItems.length === this.itemsList.length ? true : false;
+        }
+    },
+    methods: {
+        handlerSelectAll() {
+            if (this.selectedItems.length !== this.itemsList.length) {
+                this.selectedItems = [...this.itemsList];
+            } else {
+                this.selectedItems.splice(0);
+            }
+        }
+    },
+    template: `
+        <div id="complexList" class="complex-list complex-list__filter bootstrap-select">
+            <button class="btn dropdown-toggle position-relative text-left d-flex align-items-center"
+                type="button"   
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false">
+                <slot name="label"></slot>
+                <p class="d-flex mb-0">
+                    <span class="font-h5">{{ title }}</span>  
+                    <span class="complex-list_filled ml-1 text-gray-600">({{ selectedItems.length }}/{{ itemsList.length }} selected)</span>
+                </p>
+            </button>
+            <div class="dropdown-menu close-outside"
+                style="min-width: auto">
+                <div v-if="itemsList.length > 4" class="px-3 pb-2 search-group">
+                    <div class="custom-input custom-input_search__sm position-relative">
+                        <input
+                            type="text"
+                            placeholder="Search"
+                            v-model="inputSearch">
+                        <img src="assets/ico/search.svg" class="icon-search position-absolute">
+                    </div>
+                </div>
+                <ul class="my-0">
+                    <li
+                        class="dropdown-item dropdown-menu_item d-flex align-items-center">
+                        <label
+                            class="mb-0 w-100 d-flex align-items-center custom-checkbox"
+                            :class="{ 'custom-checkbox__minus': isAllSelected }">
+                            <input
+                                @click="handlerSelectAll"
+                                :ref="refSearchId"
+                                type="checkbox">
+                            <span class="w-100 d-inline-block ml-3">All</span>
+                        </label>
+                    </li>
+                    <li
+                        class="dropdown-item dropdown-menu_item d-flex align-items-center"
+                        v-for="item in foundItems" :key="item.id">
+                        <label
+                            class="mb-0 w-100 d-flex align-items-center custom-checkbox">
+                            <input
+                                :value="item"
+                                v-model="selectedItems"
+                                type="checkbox">
+                            <span class="w-100 d-inline-block ml-3">{{ item.title }}</span>
+                        </label>
+                    </li>
+                </ul>
+                <div class="p-3">
+                    <button class="btn btn-basic mr-2" type="submit">Primary</button>
+                    <button type="button" class="btn btn-secondary">Secondary</button>
+                </div>
+            </div>
+        </div>`
+};
+
 const VDropdown = {
     props: ['items'],
     data() {
@@ -566,6 +661,7 @@ const dropdownsApp = Vue.createApp({
         'complex-list-filter': ComplexListFilter,
         'removable-filter': RemovableFilter,
         'v-dropdown': VDropdown,
+        'multiselect-filter': MultiselectFilter,
     }
 });
 
