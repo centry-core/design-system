a45d29417d3eb813734b8be6fc8bbf36a74b08f9
Aspect13 <ad13box@gmail.com>
2022-11-16 17:55:09 +0400

    fixing texttoggle restyling analysis page

--- static/assets/js/vue_components/TextToggle.js
+++ static/assets/js/vue_components/TextToggle.js
@@ -1,7 +1,11 @@
+const get_hash = str => str.split('').reduce((a, b) => (((a << 5) - a) + b.charCodeAt(0)) | 0, 0)
 const TextToggle = {
     delimiters: ['[[', ']]'],
     emits: ['update:modelValue'],
     props: {
+        modelValue: {
+            type: [String, Number]
+        },
         labels: {
             type: Array,
             required: true
@@ -10,9 +14,6 @@ const TextToggle = {
             type: [Number, String],
             default: null,
         },
-        // force_html: {
-        //     type: Boolean,
-        // },
         disabled_indexes: {
             type: Array,
             default: []
@@ -21,31 +22,23 @@ const TextToggle = {
             type: Boolean,
             default: false
         },
-        // container_class: {
-        //     type: String,
-        //     default: ''
-        // },
-        // button_class: {
-        //     type: String,
-        //     default: 'btn-select dropdown-toggle'
-        // },
-        // variant: {
-        //     type: String,
-        //     default: 'with_selected',
-        //     validator(value) {
-        //         // The value must match one of these strings
-        //         return ['with_selected', 'slot'].includes(value)
-        //     }
-        // },
+        radio_group_name: {
+            type: String,
+            default: undefined
+        },
     },
     data() {
         return {
-            selected_value: null
+            selected_value: null,
+            return_value: null
         }
     },
     mounted() {
-        if (this.pre_selected_index !== null && this.pre_selected_index <= this.labels.length - 1) {
-            this.selected_value = this.get_value(this.formatted_labels[this.pre_selected_index])
+        if (this.modelValue !== undefined) {
+            this.selected_value = this.find_by_value(this.modelValue)
+        } else if (this.pre_selected_index !== null && this.pre_selected_index <= this.labels.length - 1) {
+            this.selected_value = this.formatted_labels[this.pre_selected_index]
+            // this.selected_value = this.find_by_value(this.pre_selected_index)
             // this.selected_value = this.return_ids ? this.formatted_labels[this.pre_selected_index].idx
             //     : this.formatted_labels[this.pre_selected_index].name
             // this.selected_value = this.formatted_labels[this.pre_selected_index]
@@ -64,11 +57,29 @@ const TextToggle = {
                 }
                 return {name: i, idx: index}
             })
+        },
+        radiogroup_name() {
+            if (this.radio_group_name) {
+                return this.radio_group_name
+            } else if (this.instance_name) {
+                return `options_${this.instance_name}`
+            } else {
+                return get_hash(this.formatted_labels.reduce((a ,b) => `${a.name}${b.name}`))
+            }
         }
     },
     watch: {
-        selected_value(newValue) {
+        return_value(newValue) {
             this.$emit('update:modelValue', newValue)
+        },
+        selected_value(newValue) {
+            this.return_value = this.get_value(newValue)
+        },
+        modelValue(newValue) {
+            this.selected_value = this.find_by_value(newValue)
+        },
+        return_ids(newValue) {
+            this.return_value = this.get_value(this.selected_value)
         }
     },
     methods: {
@@ -80,19 +91,29 @@ const TextToggle = {
             return attrs
         },
         get_value(item) {
-            return this.return_ids ? item.idx : item.name
+            if (item !== null) return this.return_ids ? item.idx : item.name
+            return item
+        },
+        find_by_value(value) {
+            return this.formatted_labels.find(i => {
+                if (this.return_ids) {
+                    return i.idx == value
+                } else {
+                    return i.name === value
+                }
+            })
         }
     },
-    // :value="return_ids ? i.idx : i.name"
     template: `
         <div class="btn-group btn-group-toggle" data-toggle="buttons">
             <label class="btn btn-toggle" 
                 v-for="i in formatted_labels" :key="i.idx" 
                 :class="{disabled: disabled_indexes.includes(i.idx)}"
             >
-                <input type="radio" name="text_toggle" 
+                <input type="radio" 
+                    :name="radiogroup_name" 
                     v-model="selected_value" 
-                    :value="get_value(i)"
+                    :value="i"
                     v-bind="get_disabled(i)"
                 >
                 <span v-if="i.html" v-html="i.html"></span>
