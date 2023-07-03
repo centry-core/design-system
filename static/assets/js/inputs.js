f51a432ed5f3cdc6f8188220ac85c7eea0480941
Aspect13 <ad13box@gmail.com>
2023-07-03 14:40:21 +0400

    rabbitmq fix + retention side fixes

--- static/assets/js/inputs.js
+++ static/assets/js/inputs.js
@@ -1,16 +1,25 @@
 const InputStepper = {
-    props: ['defaultValue', 'instance_name', 'uniq_id'],
-    emits: ['change'],
+    props: ['defaultValue', 'instance_name', 'uniq_id', 'modelValue', 'disabled', 'min_value'],
+    emits: ['change', 'update:modelValue'],
     data() {
         return {
-            counter: this.defaultValue
+            counter: this.defaultValue || this.modelValue
+        }
+    },
+    computed: {
+        input_min_value() {
+            return this.min_value === undefined ? 0 : this.min_value
         }
     },
     watch: {
         counter(newValue) {
             this.$emit('change', newValue)
+            this.$emit('update:modelValue', newValue)
         },
         defaultValue(newValue) {
+            this.counter = newValue
+        },
+        modelValue(newValue) {
             this.counter = newValue
         }
     },
@@ -22,15 +31,16 @@ const InputStepper = {
         }
     },
     template: `
-        <div class="input-stepper">
+        <div class="input-stepper" :class="{'disabled': disabled}">
             <button
                 @click="counter--"
-                :disabled="counter <= 0"
+                :disabled="disabled || counter <= input_min_value"
             ><i class="icon__16x16 icon-minus__16"></i>
             </button>
-            <span role="textbox" contenteditable :id="uniq_id" @input="changeCounter">{{ counter }}</span>
+            <span role="textbox" :contenteditable="!disabled" :id="uniq_id" @input="changeCounter">{{ counter }}</span>
             <button
                 @click="counter++"
+                :disabled="disabled"
             ><i class="icon__16x16 icon-plus__16"></i></button>
         </div>
     `
